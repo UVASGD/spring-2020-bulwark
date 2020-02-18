90ce483f2a3db546904836e53704128ce6c43867
Ryan Kann <rak3me@virginia.edu>
2020-02-18 16:07:41 -0500

    .gitignore potential fix

---
+++ Bulwark/Assets/Scripts/Editor/CreateTowerWindow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CreateTowerWindow : EditorWindow {
+    new string name = "New Tower";
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+    Mesh towerBase;
+    Mesh towerGun;
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Bulwark/Create Tower")]
+    static void Init () {
+        // Get existing open window or if none, make a new one:
+        CreateTowerWindow window = (CreateTowerWindow)GetWindow(typeof(CreateTowerWindow));
+        window.Show();
+    }
+
+    void OnGUI () {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+        name = EditorGUILayout.TextField("Name", name);
+        towerBase = (Mesh)EditorGUILayout.ObjectField("Tower Base Model", towerBase, typeof(Mesh));
+        towerGun = (Mesh)EditorGUILayout.ObjectField("Tower Gun Model", towerGun, typeof(Mesh));
+        //EditorGUILayout.b
+    }
+}
